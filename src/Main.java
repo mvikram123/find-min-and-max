bd762e3199c46bb452b9fd7cd5a6fe9398cb69c7
manbodhvikram <manbodhvikram@gmail.com>
2023-06-23 13:36:44 +0530

    max-min

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+// find minimum and maximum no in unsorted array
+public class Main{
+    public static void main(String args[])
+    {
+        int arr[]={4,5,1,6,7,8,3};
+        int max=0;
+        int min=Integer.MAX_VALUE;
+        for(int i=0; i<arr.length; i++)
+        {
+            max=Math.max(arr[i],max);
+            min=Math.min(arr[i],min);
+        }
+        System.out.println("maximum number is "+max);
+        System.out.println("minimum number is  "+min);
+    }
+}
